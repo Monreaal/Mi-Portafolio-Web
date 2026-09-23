5493b87942be63dee103ac29b137b8b025ca3b93
Joaquin Monreal <joaquinomonreal@gmail.com>
2026-09-22 20:17:44 -0600

    Nav y carrusel

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
 from livereload import Server
+app = Flask(__name__)
 
+@app.route('/')
+def inicio():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+"""
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,3 +29,4 @@ if __name__ == '__main__':
     
     # Levantamos el servidor exponiéndolo a tu red local
     server.serve(host='0.0.0.0', port=5000)
+    """
